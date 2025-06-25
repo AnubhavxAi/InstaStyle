558a6c583f3cd4ec095eb08539e5ff2ee6c11933
AnubhavxAi <svermaanubhav3@gmail.com>
2025-06-25 23:23:42 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,19 +1,2 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Feed from './pages/Feed';
-import Upload from './pages/Upload';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/upload" element={<Upload />} />
-      </Routes>
-    </Router>
-  );
-}
-export default App;
+import React from 'react';
+export default function App() { return <h1>Hello, InstaStyle!</h1>; }
